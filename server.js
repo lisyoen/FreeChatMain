d45578f96a159882f499d952d448ac1ac26469ac
Changyeon Lee <lisyoen@gmail.com>
2013-04-30 15:13:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,48 @@
 // server side
+var app = require('http').createServer(handler)
+    , io = require('socket.io').listen(app)
+    , fs = require('fs')
+    , exec = require('child_process').exec;
+
+app.listen(8000);
+
+function handler (req, res) {
+    res.writeHead(404);
+    res.end("Invalid Access");
+}
+
+io.sockets.on('connection', function (socket) {
+    socket.on('my other event', function (data) {
+		console.log(data);
+		socket.emit('my_message', data);
+		socket.broadcast.emit("message", data);
+	});
+
+	socket.on('report', function (data) {
+		console.log('request report');
+		socket.emit('my_report', {users: 10, rooms: 3});
+	});
+
+	function echo_exec(cmd, callback) {
+		exec(cmd, function(err, stdout, stderr) {
+			if (err) {
+				console.log(stderr + '\nError:' + err.code, err.code);
+			} else {
+				console.log(stdout);
+			}
+			socket.emit('system report', {err: err, stdout: stdout, stderr: stderr});
+
+			callback(arguments);
+		});
+	}
+
+	socket.on('git pull', function (data) {
+		console.log('git pull');
+		echo_exec('git pull', function() {});
+	});
+
+	socket.on('restart', function (data) {
+		console.log('restart');
+		echo_exec('forever restart chat.js', function() {});
+	});
+});
