ece8d5dede6ed50aff17c70504554dd38b4738f4
lisyoen <lisyoen@gmail.com>
2013-05-09 10:20:42 +0900

    something changes

--- server.js
+++ server.js
@@ -1,37 +1,61 @@
 // server side
 var app = require('http').createServer(handler)
     , io = require('socket.io').listen(app)
+	, util = require('util')
     , fs = require('fs')
     , exec = require('child_process').exec
     , qs = require('querystring');
 
 app.listen(8000);
 
-function service(id, name, desc, count) {
+function service(id, name, desc, url, count) {
 	this.id = id;
 	this.name = name;
 	this.desc = desc;
+	this.url = url;
 	this.count = count;
 }
 
-function serviceGroup(id, name) {
-	this.id = id;
-	this.name = name;
+function serviceManager(defaultLife) {
 	var services = {};
 	var size = 0;
+	var checkInterval = 1000;
+	
+	var onlineHandlers = [];
+	var offlineHandlers = [];
 	
 	// service control methods
 	this.getService = function(id) {
 		return services[id];
 	};
 	
+	this.getServices = function() {
+		return services;
+	};
+	
 	this.setService = function(id, service) {
 		size++;
-		return services[id] = service;
+		console.log('setService');
+		console.log(service);
+		if (!service.life) {
+			service.life = defaultLife;
+		}
+		if (!services[id]) {
+			services[id] = service;
+			for (var h in onlineHandlers) {
+				onlineHandlers[h].apply(this, service);
+			}
+		}
+		else {
+			services[id] = service;
+		}
+		return service;
 	};
 	
 	this.removeService = function(id) {
-		size--;
+		if (services[id]) {
+			size--;
+		}
 		return delete services[id];
 	};
 	
@@ -40,53 +64,107 @@ function serviceGroup(id, name) {
 		services = {};
 	};
 	
-	this.forEach = function(cb) {
+	this.forEach = function(callback) {
 		for (var i in services) {
-			cb.call(this, services[i]);
+			callback.call(this, services[i]);
+		}
+	};
+	
+	var that = this;
+	setInterval(function() {
+		for (var id in services) {
+			var svc = services[id];
+			if (--svc.life <= 0) {
+				for (var h in offlineHandlers) {
+					offlineHandlers[h].apply(that, svc);
+				}
+				that.removeService(id);
+			}
+		}
+	}, checkInterval);
+	
+	this.size = function() {
+		return size;
+	};
+	
+	this.on = function(event, handler) {
+		switch(event) {
+			case 'add':
+			case 'online': {
+				onlineHandlers.push(handler);
+				break;
+			}
+			case 'remove':
+			case 'offline': {
+				offlineHandlers.push(handler);
+				break;
+			}
+			default: {
+				break;
+			}
 		}
 	};
 }
 
-var sg = new serviceGroup('chat', 'Chatting');
+var sm = new serviceManager(10);
+
+function refresh() {
+	io.sockets.emit('services', sm.getServices());
+	console.log('refresh');
+	console.log(sm.getServices());
+}
+
+sm.on('online', function(service) {
+	console.log('online');
+	console.log(service);
+	refresh();
+});
+
+sm.on('offline', function(service) {
+	console.log('offline');
+	console.log(service);
+	refresh();
+});
 
 function handler (req, res) {
 	console.log(req.url);
 	switch (req.url)
 	{
-	case '/createService':
-	{
-		if (req.method !== 'POST') {
+		case '/createService': {
+			if (req.method !== 'POST') {
+				res.writeHead(400);
+				res.end('Bad Request');
+				return;
+			}
+			var data = '';
+			req.setEncoding('utf8');
+			req.on('data', function(chunk) {
+				data += chunk;
+			});
+			
+			req.on('end', function() {
+				var q = qs.parse(data);
+				if (q.id && q.name) {
+					sm.setService(q.id, new service(q.id, q.name, q.desc, q.url, q.count));
+					res.writeHead(200);
+					res.end('Ok');
+				}
+				else {
+					res.writeHead(400);
+					res.end('Bad Request');
+				}
+			});
+			break;
+		}
+		default: {
 			res.writeHead(400);
 			res.end('Bad Request');
-			return;
+			break;
 		}
-		var data = '';
-		req.setEncoding('utf8');
-		req.on('data', function(chunk) {
-			data += chunk;
-		});
-		
-		req.on('end', function() {
-			var json = qs.parse(data);
-			console.log('data: ' + data);
-			console.log('json: ' + json);
-		});
-	}
-	default:
-	{
-		res.writeHead(400);
-		res.end('Bad Request');
-	}
-	break;
 	}
 }
 
 io.sockets.on('connection', function (socket) {
-	socket.on('report', function (data) {
-		console.log('request report');
-		socket.emit('my_report', {users: 10, rooms: 3});
-	});
-
 	function echo_exec(cmd, callback) {
 		exec(cmd, function(err, stdout, stderr) {
 			if (err) {
